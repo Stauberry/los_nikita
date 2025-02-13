bec073dec18d6a5d13d1daeb9a789bf8b2165b58
Stauberry <niki.37502>
2025-02-13 11:40:06 +0300

    Домашка Урок 14

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
 <html>
 <?php
+//</editor-fold desc="на всякий случай">
+ini_set("MEMORY_LIMIT", "128M");
+echo "<pre>";
 
-echo '<pre>';
-// комментарий 1 строка
-/* комментарий 2 абзац */
-//</editor-fold desc="комментарий 3 особый">
-
+/* функция разделения для удобного чтения */
 function newTitle($number, $title)
 {
-    echo '<br>' . $number . " " . $title . '<br>';
+    echo "<br>" . $number . " " . $title . "<br>";
 }
+
 newTitle(1, "Приветствие");
 
 /* Приветствие */
-function sayHello($a) {
+function sayHello($a)
+{
     if (!is_int($a)) {
         echo "Передался не целочисленный параметр\n";
         return;
@@ -22,8 +23,9 @@ function sayHello($a) {
         echo "hello world!\n";
     }
 }
+
 sayHello(5);
-sayHello("5"); // Ошибка
+sayHello("5"); // Выдаст ошибку
 newTitle(2, "Сумма");
 
 /* Сложение цен */
@@ -31,7 +33,8 @@ function returnPrice($firstPrice, $secondPrice)
 {
     return $firstPrice + $secondPrice;
 }
-echo 'Сумма товара = ' . returnPrice(2 ,3);
+
+echo 'Сумма товара = ' . returnPrice(2, 3);
 newTitle(3, "Вывод имени");
 
 /**
@@ -44,23 +47,25 @@ function greeting(&$name): void
     $name = "Привет, $name!" . PHP_EOL;
     echo $name;
 }
+
 $name = 'Nik';
 greeting($name);
 newTitle(4, "Анонимная функция умножения на 2");
 
-$multiply = function (int $num) {echo $num * 2 . "\n";};
+$multiply = function (int $num) {
+    echo $num * 2 . "\n";
+};
 $multiply(2);
 $multiply(4);
 newTitle(5, "Тоже самое но со стрелочной функцией");
-echo "ошибка";
-/* что то не так....хотя вроде работает
+
 $secret = fn($num) => $num * 2;
 
 echo $secret(3) . "\n";
 echo $secret(5) . "\n";
-*/
 
-/* 2ой вариант
+
+/* 2ой вариант */
 $secret = fn($num) => fn($snum) => $num * $snum;
 
 $multiplyBy3 = $secret(3);
@@ -68,25 +73,90 @@ echo $multiplyBy3(5) . "\n";
 
 $multiplyBy5 = $secret(5);
 echo $multiplyBy5(2) . "\n";
-*/
+
 
 newTitle(6, "рекусривная функция обхода многомерного массива");
 
 /* вывод массива */
 $array = [
-    ["Nik", 27, 3700],
-    ["Alex", 23, 470],
-    ["Steve", 43, 3400]];
+    [
+        "name" => "Nik",
+        "age" => 27,
+        "exp" => 3700
+    ],
+    [
+        "name" => "Alex",
+        "age" => 23,
+        "exp" => 470
+    ],
+    [
+        "name" => "Steve",
+        "age" => 43,
+        "exp" => 26000
+    ]
+];
 
 function examination($matrix)
 {
-    foreach ($matrix as $item) {
-        print_r($item);
+    foreach ($matrix as $key => $value) {
+        if (is_array($value)) {
+            echo "$key:\n"; // Выводим номер массива
+            examination($value); // Вызов самой себя
+        } else {
+            echo "$key => $value\n"; // Выводим ключ и значение
+        }
     }
 }
+
 examination($array);
 newTitle(7, "array_map");
 
+$matrix = [1, 2.1, 3];
+$newArray = array_map(fn($num) => $num + 1, $matrix);
+var_dump($newArray);
+
+newTitle(8, "strlen(), strtoupper(), strtolower()");
+$Hello = "Hello World!";
+echo $strlenHello = strlen($Hello) . "<br>";
+echo $strtoupperHello = strtoupper($Hello) . "<br>";
+echo $strtolowerHello = strtolower($Hello) . "<br>";
+
+newTitle(9, "array_push(), array_pop(), array_merge()");
+
+array_push($matrix, 4, 5);
+echo "После array_push: ", implode(", ", $matrix), "<br>";
+
+echo "Удалённый элемент: ", array_pop($matrix), "<br>";
+echo "После array_pop: ", implode(", ", $matrix), "<br>";
+
+$mergedMatrix = array_merge($matrix, [6, 7, 8]);
+echo "После array_merge: ", implode(", ", $mergedMatrix);
+
+newTitle(10, "is_string(), is_numeric(), is_array()");
+
+$data = ["Hello", 123, 45.6, [1, 2, 3], true];
+
+foreach ($data as $item) {
+    var_export($item);
+    echo " | is_string: " . (is_string($item) ? "true" : "false");
+    echo " | is_numeric: " . (is_numeric($item) ? "true" : "false");
+    echo " | is_array: " . (is_array($item) ? "true" : "false");
+    echo "<br>";
+}
+
+newTitle(11,"abs(), sqrt(), round(), ceil(), floor()");
+
+$num = -9.7;
+echo "$num | abs: " . abs($num) . " | sqrt: " . sqrt(abs($num)) .
+    " | round: " . round($num) . " | ceil: " . ceil($num) .
+    " | floor: " . floor($num) . " | rand: " . rand(1, 100) .
+    " | mt_rand: " . mt_rand(1, 100);
+
+newTitle(12, "date()");
+
+echo "Дата: " . date("Y.m.d") . " | Время: " . date("H:i:s") .
+    " | Полная дата: " . date("l, d F Y H:i:s") .
+    " | Год: " . date("Y");
 
 ?>
 </html>
